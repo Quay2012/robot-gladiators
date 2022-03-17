a394e5d09127cd90dce889e9a9d34e0a2fd45a65
Quay2012 <95986124+Quay2012@users.noreply.github.com>
2022-03-17 11:40:01 -0400

    added a for loop and an array

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,15 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var playerMoney = 10;
+
+
+
+var fight = function(enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -63,10 +67,25 @@ var fight = function() {
         // if player choses to skip
     } else if (promptFight === "skip" || promptFight === "SKIP") {
         window.alert(playerName + " has chosen to skip the fight!");
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        //if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            //subtract money from playerMoney for skipping
+            playerMoney = playerMoney - 2;
+        }
+
+        // if no(false), ask question again by running fight() again
+        else {
+            fight();
+        }
     } else  {
         window.alert("You need to choose a valid option. Try again!");
     }
     }
 
-
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+fight(enemyNames[i]);
+}
