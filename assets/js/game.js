75043b216b4d8de1b149cc1711afec9536dc4e6d
Quay2012 <95986124+Quay2012@users.noreply.github.com>
2022-03-27 19:27:53 -0400

    added the math.max,floor to generate random numeric values

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,11 +12,21 @@ console.log(enemyNames);
 console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
+// prints 3.14
+console.log(Math.PI);
+// rounds to the nearest whole number(4)
+console.log(Math.round(4.4));
+// prints the square root (5)
+console.log(Math.sqrt(25));
+// prints 100
+console.log(Math.max(10, 20, 100));
+// prints 0
+console.log(Math.max(0, -50));
 
 
 
 var fight = function(enemyName) {
-    debugger;
+  
 
     // repeat and execute as long as the enemy-robot is alive
     while (enemyHealth > 0 && enemyHealth > 0) {
@@ -26,7 +36,8 @@ var fight = function(enemyName) {
         // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -51,7 +62,7 @@ var fight = function(enemyName) {
         if (confirmSkip) {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             //subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
         }
@@ -60,7 +71,7 @@ var fight = function(enemyName) {
         
 
        //Subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variale
-    enemyHealth = enemyHealth - playerAttack
+    enemyHealth = Math.max(0, enemyHealth - damage);
 
     //Log a resulting message to the console to confirm that it worked
     console.log(
@@ -80,7 +91,8 @@ var fight = function(enemyName) {
     }
 
     //Subtract the value of enemyAttack from the value of playerHealth and use that result to update the value in the playerHealth variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
 
     //Log a resulting message to the console to confirm that it worked
     console.log(
@@ -104,13 +116,13 @@ var fight = function(enemyName) {
 
     
 var startGame = function() {
-    debugger;
+    
         // reset player stats
         playerHealth = 100;
         playerAttack = 10;
         playerMoney = 10;
 
-    
+// fight each enemy robot by looping over them and fighting them one at a time    
 for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         // let user know what round they are in, remember that arrays start at 0 so
@@ -120,7 +132,8 @@ for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    // By multiplying Math.random() by 60, we've now specified a random range from 0 to 59.xx (remember, Math.random() will never be 1, so we would never get an even 60). We don't want decimal numbers cluttering up our game, though, so we can use Math.floor() to round down to the nearest whole number.
+    enemyHealth = randomNumber(40, 60);
     fight(pickedEnemyName);
 
     // if the player is still alive and we're not at the last enemy in the array
@@ -147,6 +160,7 @@ endGame();
 
 // function to end the entire game
 var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
     // if player is still alive, player wins!
 
     if (playerHealth > 0) {
@@ -155,7 +169,6 @@ var endGame = function() {
     else {
         window.alert("You've lost your robot in battle.");
     }
-    window.alert("The game has now ended. Let's see how you did!");
 
     if (playerHealth > 0 && i < enemyNames.length - 1) {
         //ask if player wants to use the store before next round
@@ -202,7 +215,7 @@ var endGame = function() {
                 break;
 
             case "leave":
-            case "CASE":    
+            case "LEAVE":    
                 window.alert("Leaving the store.");
                 
                 // do nothing, so function will end
@@ -214,9 +227,10 @@ var endGame = function() {
                     // call shop() again to force player to pick a valid option
                     shop();
                     break;
-        }
+         }
 
-    };
+       };
+    }  
 
     // ask player if they'd like to play again
 
@@ -228,6 +242,14 @@ if (playAgainConfirm) {
 }else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
-}
 
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
+
+startGame();
 
